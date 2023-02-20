bb7240090d9fd3003d2b9a6b5d9426e7e9a58100
Алексей Чайкин <barjastroy@gmail.com>
2023-02-20 16:36:16 +0300

    Initial Commit

---
+++ LogIn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LogIn
+//
+//  Created by macbook on 20.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LogIn/LogInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInApp.swift
+//  LogIn
+//
+//  Created by macbook on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct LogInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
