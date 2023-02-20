be9799b4930fdbf7af86967c9da401f65d0757ca
Алексей Чайкин <barjastroy@gmail.com>
2023-02-20 18:59:08 +0300

    Update SignUp.swift

--- LogIn/SignUp.swift
+++ LogIn/SignUp.swift
@@ -11,6 +11,9 @@ struct SignIn : View {
     
     @State var email = ""
     @State var pass = ""
+    @State var message = ""
+    @State var alert = false
+    @State var show = false
     
     var body : some View {
         VStack {
@@ -65,14 +68,32 @@ struct SignIn : View {
                 
                 Button(action: {
                     
+                    signInWithEmail(email: self.email, password: self.pass, competion: ({ (verified, status) in
+                        
+                        if !verified {
+                            
+                            self.message = status
+                            self.alert.toggle()
+                        }
+                        else {
+                            
+                            UserDefaults.standard.set(true, forKey: "status")
+                            NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+                        }
+                    }))
+                    
                 }) {
                     
                     Text("Sign In").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
                     
                 }.background(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
-                    .clipShape(Capsule())
-                    .padding(.top, 45)
-                
+                .clipShape(Capsule())
+                .padding(.top, 45)
+                .alert(isPresented: $alert, content: {
+                    
+                    Alert(title: Text("Error"), message: Text(self.message), dismissButton: .default(Text("Ok")))
+                })
+            
                 Text("(or)").foregroundColor(Color.gray.opacity(0.5)).padding(.top,30)
                 
                 HStack(spacing: 8){
