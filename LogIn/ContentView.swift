e29f8330e16d6e0eb385ccdbfe21a6688526d523
Алексей Чайкин <barjastroy@gmail.com>
2023-02-20 18:46:41 +0300

    add firebase

--- LogIn/ContentView.swift
+++ LogIn/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
 
 struct ContentView: View {
     var body: some View {
@@ -13,10 +15,32 @@ struct ContentView: View {
     }
 }
 
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func signInWithEmail(email: String, password: String, competion: @escaping (Bool, String) -> Void ) {
+    Auth.auth().signIn(withEmail: email, password: password, completion: ({ (res, err) in
+        
+        if err != nil {
+            competion(false, (err?.localizedDescription)!)
+            return
+        }
+        
+        competion(true, (res?.user.email)!)
+    }))
+}
+
+func signUpWithEmail(email: String, password: String, competion: @escaping (Bool, String) -> Void ) {
+    Auth.auth().createUser(withEmail: email, password: password, completion: ({ (res, err) in
+        
+        if err != nil {
+            competion(false, (err?.localizedDescription)!)
+            return
+        }
+        
+        competion(true, (res?.user.email)!)
+    }))
 }
