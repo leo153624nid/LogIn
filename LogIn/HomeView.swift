116ac16fddc9cca7199dab4a40e6ad357a440061
Алексей Чайкин <barjastroy@gmail.com>
2023-02-20 19:24:03 +0300

    Update HomeView.swift

--- LogIn/HomeView.swift
+++ LogIn/HomeView.swift
@@ -13,14 +13,25 @@ struct HomeView: View {
     var body: some View {
         
         VStack {
+            Spacer()
             
-            Text("Hello, \(name)!")
+            Text("Hello, \(name)!").fontWeight(.heavy).font(.largeTitle).padding(20)
+            
+            Spacer()
             
             Button(action: {
                 
+                UserDefaults.standard.set(false, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+                
             }, label: {
-                Text("LogOut")
+                
+                Text("LogOut").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
             })
+            .background(Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
+            .clipShape(Capsule())
+            
+            Spacer()
         }
         
     }
@@ -28,6 +39,6 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(name: .constant("name"))
+        HomeView(name: .constant("name@gmail.com"))
     }
 }
